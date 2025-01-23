f638a2c795dc6bd6f58da1e41e853f1d9633e6eb
davespser <davespser@gmail.com>
2025-01-23 17:04:12 +0100

    Actualización de archivos

--- src/App2.jsx
+++ src/App2.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
-import { Plane } from "@react-three/drei";
+import { Plane, OrbitControls } from "@react-three/drei";
 import * as THREE from "three";
 import { useTexture } from "@react-three/drei";
 
 const Ocean = () => {
   const waveTexture = useTexture('./textures/olas.png'); // Cargamos la textura de las olas
+
+  // Configuración para repetir la textura
+  waveTexture.wrapS = THREE.RepeatWrapping;
+  waveTexture.wrapT = THREE.RepeatWrapping;
+  waveTexture.repeat.set(10, 10); // Repetir la textura 10x10 en toda la superficie
 
   return (
     <Plane args={[100, 100, 100, 100]} rotation={[-Math.PI / 2, 0, 0]}>
@@ -14,7 +19,6 @@ const Ocean = () => {
         side={THREE.DoubleSide}
         transparent={true}
         opacity={1}
-        repeat={new THREE.Vector2(1, 1)} // Repite la textura
       />
     </Plane>
   );
@@ -30,10 +34,12 @@ const App = () => {
         top: 0,
         left: 0,
       }}
+      camera={{ position: [50, 50, 50], fov: 75 }} // Posición inicial de la cámara
     >
       <ambientLight intensity={0.5} />
       <directionalLight position={[10, 10, 5]} intensity={1} />
       <Ocean />
+      <OrbitControls /> {/* Cámara orbital */}
     </Canvas>
   );
 };
